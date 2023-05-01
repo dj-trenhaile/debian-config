fbc8fdf2425b0eb13b3f71a911014b67772c905c
Danvers Trenhaile <djtrenhaile@gmail.com>
2023-05-01 13:12:13 -0500

    (fix) eco mode get_state: polybar-msg output overriding hook echo

--- fs/home/USER/.scripts/eco_mode/get_state.sh
+++ fs/home/USER/.scripts/eco_mode/get_state.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# loading
+echo "initializing..."
+
 if [ "$(powerprofilesctl get)" == "performance" ]
 then
-    polybar-msg hook ecomode 2
+    polybar-msg hook ecomode 2 &> /dev/null
 else
     pkill -9 picom  # in case latent process exists; throws error on toggle if not killed here
-    polybar-msg hook ecomode 3
+    polybar-msg hook ecomode 3 &> /dev/null
 fi
     
 
