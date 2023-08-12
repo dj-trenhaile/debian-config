ee58f184dda5d49e6f43c2c4edc0bccb06dbced8
Danvers Trenhaile <djtrenhaile@gmail.com>
2023-08-12 13:52:13 -0500

    bug fixes ecomode

--- src/home/USER/.polybar/modules/ecomode/profiles.sh
+++ src/home/USER/.polybar/modules/ecomode/profiles.sh
@@ -2,11 +2,11 @@
 
 power-saver() {
     ~/.i3/power_profiles/power-saver.sh
-    polybar-msg hook ecomode 3
+    polybar-msg hook ecomode 3 &> /dev/null
 }
 
 balanced() {
     ~/.i3/power_profiles/balanced.sh
     flock -u 9  # open file descriptor passed on to picom instance; unlock explicitly
-    polybar-msg hook ecomode 2
+    polybar-msg hook ecomode 2 &> /dev/null
 }
