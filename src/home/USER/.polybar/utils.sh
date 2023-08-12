24648dbe7038434af59d9c9619dde211d3fbb9fa
Danvers Trenhaile <djtrenhaile@gmail.com>
2023-08-12 13:12:08 -0500

    polybar util fix

--- src/home/USER/.polybar/utils.sh
+++ src/home/USER/.polybar/utils.sh
@@ -7,27 +7,14 @@
 get_num_chars() {
     DISPLAY_ID=$1
     DISPLAY_FRACTION=$2
-    FONT_WIDTH=$3
+    FONT_WIDTH=$3  # measured in characters per pixel
 
     display_header=$(xrandr | grep $DISPLAY_ID)    
-    display_dims=$(echo $display_header | tr ' ' '\n' | grep mm)
-
-    # get display width (mm)
-    display_props=$(echo $display_header | cut -d '(' -f1)
-    # echo $display_props
-    # echo $display_props | grep "right"
-    if [ "$(echo $display_props | grep "right")" == "" ] && \
-       [ "$(echo $display_props | grep "left")" == "" ]
-    then 
-        width_mm=$(echo -e "$display_dims" | head -n 1)
-    else
-        width_mm=$(echo -e "$display_dims" | tail -n 1)
-    fi
-    width_mm=$(echo $width_mm | tr -d 'm')
+    display_pixel_width=$(xrandr | grep DP-2 | cut -d 'x' -f1 | tr ' ' '\n' | tail -n 1)
 
     # convert width to pts, scale by fraction, and convert to num of chars;
     # 1 in / 24.5 mm, 72 pts / in, 1 char / FONT_WIDTH pts
-    num_chars=$(bc -l <<< "((${width_mm} / 25.4) * 72 * ${DISPLAY_FRACTION}) / ${FONT_WIDTH}")  
+    num_chars=$(bc -l <<< "${FONT_WIDTH} * ${display_pixel_width} * ${DISPLAY_FRACTION}")  
 
     # return num_chars floored to integer
     echo $(bc <<< "scale=0; ${num_chars} / 1")
@@ -35,18 +22,12 @@ get_num_chars() {
 
 # get_num_chars wrapper for 26-pt monospaced font
 get_num_chars_26_mono() {
-    FONT_WIDTH=8.39
+    FONT_WIDTH=0.053
     get_num_chars $1 $2 $FONT_WIDTH
 }
 
 # get_num_chars wrapper for 20-pt monospaced font
 get_num_chars_20_mono() {
-    FONT_WIDTH=6.62
-    get_num_chars $1 $2 $FONT_WIDTH
-}
-
-# get_num_chars wrapper for 18-pt monospaced font
-get_num_chars_18_mono() {
-    FONT_WIDTH=5.75
+    FONT_WIDTH=0.067
     get_num_chars $1 $2 $FONT_WIDTH
 }
