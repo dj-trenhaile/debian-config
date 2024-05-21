dc41943118c7eed4a1def2ac050c23de513762c6
Danvers Trenhaile <djtrenhaile@gmail.com>
2024-05-20 23:32:55 -0500

    polybar extra bars cleanup

--- src/home/USER/.polybar/extra_bars/launch.sh
+++ src/home/USER/.polybar/extra_bars/launch.sh
@@ -2,10 +2,11 @@
 
 # set variables
 set -a
-source $POLYBAR_VARS
+source $_POLYBAR_VARS
 set +a
 export BAR_MONITOR=$(get-current-display)
-echo "monitor: ${BAR_MONITOR}"
+echo "monitor: $BAR_MONITOR"
 
 # launch bar
 polybar extras-secondary &
+
--- src/home/USER/.polybar/extra_bars/terminate.sh
+++ src/home/USER/.polybar/extra_bars/terminate.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-current_display=$(get-current-display)
-
 while read process; do
     pid=$(echo $process | cut -d ' ' -f3)
-    if [ "$(strings /proc/${pid}/environ | grep BAR_MONITOR | cut -d '=' -f2)" == $current_display ]; then
+    if [ "$(strings /proc/$pid/environ | grep BAR_MONITOR | cut -d = -f2)" == "$(get-current-display)" ]; then
         kill $pid
     fi
-done < <(ps -C polybar -o args,pid= | grep "extras-secondary" | tr -s ' ')
+done < <(ps -C polybar -o args,pid= | grep extras-secondary | tr -s ' ')
+
