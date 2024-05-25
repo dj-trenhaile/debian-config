dd394d38bb1ea94bc73c9fb34e89d06509a54c6d
Danvers Trenhaile <djtrenhaile@gmail.com>
2024-05-24 20:39:43 -0500

    made polybar bluetooth module event-driven

--- src/home/USER/.polybar/modules/bluetooth/get_status.sh
+++ src/home/USER/.polybar/modules/bluetooth/get_status.sh
@@ -1,12 +1,46 @@
 #!/bin/bash
+_WATCH_EXP="type=signal, \
+            path=/org/blueman/Applet, \
+            interface=org.blueman.Applet, \
+            member=IconNameChanged"
+_MSG_PREFIX='string "blueman-'
+
+
+print_disabled() {
+    echo "%{F$COLOR_DISABLED}%{F-}"
+}
+
+print_disconnected() {
+    echo 
+}
+
+print_connected() {
+    echo '%{F#2193ff}%{F-}'
+}
+
 
 if [ "$(bluetoothctl show | grep 'Powered: yes')" == '' ]; then
-  echo "%{F$COLOR_DISABLED}%{F-}"
+    print_disabled
 else
-  if [ "$(bluetoothctl info | grep 'DeviceSet (null)')" == '' ]; then
-    echo '%{F#2193ff}%{F-}'
-  else
-    echo 
-  fi
+    if [ "$(bluetoothctl info | grep 'DeviceSet (null)')" == '' ]; then
+        print_connected
+    else
+        print_disconnected
+    fi
 fi
+
+
+while read event_line; do
+    case $event_line in
+        "${_MSG_PREFIX}disabled\"")
+            print_disabled
+            ;;
+        "${_MSG_PREFIX}tray\"")
+            print_disconnected
+            ;;
+        "${_MSG_PREFIX}active\"")
+            print_connected
+            ;;
+    esac
+done < <(dbus-monitor "$_WATCH_EXP")
 
