9113668019cf3ad535ad6ca825ca59fcc441af89
Danvers Trenhaile <djtrenhaile@gmail.com>
2024-06-01 16:04:16 -0500

    reformatted lock_screen.sh

--- src/home/USER/.i3/lock_screen.sh
+++ src/home/USER/.i3/lock_screen.sh
@@ -62,20 +62,24 @@ fi
 
 # take transformed scrot ===================================================== #
 
-res=$(xrandr | grep current | sed -E 's/.*current\s([0-9]+)\sx\s([0-9]+).*/\1x\2/')  # TODO: break out
-cmd="ffmpeg -f x11grab -video_size $res -y -i $DISPLAY"
+# init cmd
+current_screen=$(xrandr | grep current)
+cmd="ffmpeg -f x11grab -video_size $(echo $current_screen | cut -d ' ' -f8)x\
+$(echo $current_screen | cut -d ' ' -f10 | cut -d ',' -f1) -y -i $DISPLAY"
+
+# init filter
 filter='[0]boxblur=10:1'
 
 overlay_num=1
 while read overlay_offsets; do
+    # add another overlay to ffmpeg cmd
+    cmd="$cmd -i $_OVERLAY"
+    
     # read overlay offsets
     x_offset=$(echo $overlay_offsets | cut -d x -f1)
     y_offset=$(echo $overlay_offsets | cut -d x -f2)
-    
-    # add a new image to ffmpeg command
-    cmd="$cmd -i $_OVERLAY"
   
-    # add offsets to ffmpeg filter
+    # add overlay offsets to ffmpeg filter
     filter_stage="[o$overlay_num]"
     filter="$filter${filter_stage}\;$filter_stage[$overlay_num]overlay=$x_offset:$y_offset"
   
@@ -83,7 +87,6 @@ while read overlay_offsets; do
 done < <(cat $_OVERLAY_OFFSETS)
 
 cmd="$cmd -filter_complex $filter -vframes 1 $_BG -loglevel quiet"
-echo $cmd
 eval $cmd
 
 # ======================== #
