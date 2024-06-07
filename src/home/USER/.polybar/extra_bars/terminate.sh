47a41cc2c1bdbd8617d9af03ee25054681837cc8
Danvers Trenhaile <djtrenhaile@gmail.com>
2024-06-06 23:16:37 -0500

    updated polybar extra bars terminate.sh

--- src/home/USER/.polybar/extra_bars/terminate.sh
+++ src/home/USER/.polybar/extra_bars/terminate.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 while read process; do
-    pid=$(echo $process | cut -d ' ' -f3)
+    pid=${process##* }
     if [ "$(strings /proc/$pid/environ | grep BAR_MONITOR | cut -d = -f2)" == "$(get-current-display)" ]; then
         kill $pid
     fi
-done < <(ps -C polybar -o args,pid= | grep extras-secondary | tr -s ' ')
+done < <(ps -C polybar -o args:100,pid= | grep extras-secondary)
